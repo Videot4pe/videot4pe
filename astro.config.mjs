3aff8e9270f71144ba3e7c2d03d9c938413d47a0
Alexandr Kvasnikov <videot4pe@gmail.com>
2023-03-15 17:40:42 +0300

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import image from "@astrojs/image";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://videot4pe.github.io',
+  // base: '/videot4pe',
+  integrations: [
+    image({serviceEntryPoint: "@astrojs/image/sharp"}), mdx(), sitemap()
+  ]
+});
---
+++ src/content/blog/test.md
@@ -0,0 +1,15 @@
+---
+draft: false
+title: "Test post"
+snippet: "test snippet."
+image: {
+    src: "https://images.unsplash.com/photo-1667372393119-3d4c48d07fc9?&fit=crop&w=430&h=240",
+    alt: "test"
+}
+publishDate: "2023-03-21 10:00"
+category: "Test"
+author: "Videot4pe"
+tags: [videot4pe]
+---
+
+test text
